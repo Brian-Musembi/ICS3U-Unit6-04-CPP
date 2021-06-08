1e4b1345b39a3f83454c81a36ec722bc11e94796
Brian Musembi <brian.musembi@mths.ca>
2021-06-08 02:18:36 +0000

    Added array_average.cpp

--- array_average.cpp
+++ array_average.cpp
@@ -7,17 +7,19 @@
 #include <iostream>
 #include <random>
 
+
 template<int rows, int columns>
-float AvgOfNumbers(int (&array)[rows][columns]) {
-    // this function finds the average among the 2d list
+float average2D(int (&array)[rows][columns]) {
+    // this function finds the average
+
     float total = 0;
-    int counterRow = 0;
-    int counterCol = 0;
+    int numOfRows = 0;
+    int numOfCols = 0;
     float average;
 
-    for (int counterRow = 0; counterRow < rows; counterRow++) {
-        for (int counterCol = 0; counterCol < rows; counterCol++) {
-            total += array[counterRow][counterCol];
+    for (int numOfRows = 0; numOfRows < rows; numOfRows++) {
+        for (int numOfCols = 0; numOfCols < rows; numOfCols++) {
+            total += array[numOfRows][numOfCols];
         }
     }
     average = total / (rows * columns);
@@ -32,29 +34,29 @@ int main() {
               << std::endl;
     std::cout << "" << std::endl;
 
-    // declaration
-    const int rows = 4;  // constant
-    const int columns = 5;  // constant
-    int array[rows][columns];  // array
-    int singleNumber;  // variable
+    // variables
+    int randomNum;
+    const int rows = 4;
+    const int columns = 5;
+    int array[rows][columns];
 
-    // process -- generate random numbers
+    // creating 2D array
     for (int rowCounter = 0; rowCounter < rows; rowCounter++) {
-        std::cout;  // output
+        std::cout;
         for (int colCounter = 0; colCounter < columns; colCounter++) {
             std::random_device rseed;
-            std::mt19937 rgen(rseed());  // mersenne_twister
-            std::uniform_int_distribution<int> idist(0, 50);  // [1,100]
-            singleNumber = idist(rgen);
-            array[rowCounter][colCounter] = singleNumber;
-            std::cout << singleNumber << ", ";  // output
+            std::mt19937 rgen(rseed());
+            std::uniform_int_distribution<int> idist(0, 50);
+            randomNum = idist(rgen);
+            array[rowCounter][colCounter] = randomNum;
+            std::cout << randomNum << ", ";
         }
-    std::cout << std::endl;  // output
+    std::cout << std::endl;
     }
 
     // output
-    float average = AvgOfNumbers(array);
+    float average = average2D(array);
     std::cout << "" << std::endl;
-    std::cout << "The average of all the numbers is: " 
+    std::cout << "The average of all the numbers is: "
               << average << std::endl;
 }
